9685cc5ccac4446e661068d77ee17b6fc23c8db8
mishan3581 <mishan358.h@gmail.com>
2025-04-17 15:44:09 +0300

    d

--- tests/scripts/run_vrunner.bat
+++ tests/scripts/run_vrunner.bat
@@ -1,5 +1,5 @@
 @echo off
 chcp 65001
-if "%1"=="session_kill" call vrunner session kill --settings tests/tools/vrunner.json
-if "%1"=="loadrepo" call vrunner loadrepo --settings %2
-if "%1"=="updatedb" call vrunner updatedb --settings %2
+if "%1"=="session_kill" call vrunner session kill --settings    tests/tools/vrunner.json
+if "%1"=="loadrepo" call vrunner loadrepo --settings            tests/tools/vrunner.json
+if "%1"=="updatedb" call vrunner updatedb --settings            tests/tools/vrunner.json
