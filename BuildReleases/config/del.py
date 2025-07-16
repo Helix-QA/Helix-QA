98360af83b407e45d73c1a54f80fa78439b5ac21
mishan3581 <mishan358.h@gmail.com>
2025-07-16 14:51:17 +0300

    builreleases

---
+++ BuildReleases/config/del.py
@@ -0,0 +1,23 @@
+import shutil
+import os
+import sys
+
+archive_name = sys.argv[1]
+# Указываем путь
+path = rf"{archive_name}\Build"
+
+# Проверяем, существует ли путь
+if os.path.exists(path):
+    # Получаем список всех элементов в директории
+    for item in os.listdir(path):
+        item_path = os.path.join(path, item)
+        # Проверяем, является ли элемент папкой
+        if os.path.isdir(item_path):
+            try:
+                # Удаляем папку и всё её содержимое
+                shutil.rmtree(item_path)
+                print(f"Удалена папка: {item_path}")
+            except Exception as e:
+                print(f"Ошибка при удалении {item_path}: {str(e)}")
+else:
+    print(f"Указанный путь не существует: {path}")
