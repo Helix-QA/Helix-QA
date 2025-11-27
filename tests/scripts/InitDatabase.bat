79b5912baa26541192f488422963f9ac04597db5
mishan3581 <mishan358.h@gmail.com>
2025-11-27 12:46:25 +0200

    в

--- tests/scripts/InitDatabase.bat
+++ tests/scripts/InitDatabase.bat
@@ -2,7 +2,7 @@
 chcp 65001
 
 REM Переходим к нужной функции
-if "%1"=="session_kill" call vrunner session kill --ras serverRAC --db %2 --db-user Админ --uccode tester
+if "%1"=="session_kill" call vrunner session kill --ras "C:\Program Files\1cv8\8.3.27.1688\bin\rac.exe"--db %2 --db-user Админ --uccode tester
 if "%1"=="loadrepo"     call vrunner loadrepo --storage-name %2 --storage-user %3 --ibconnection /Slocalhost/%4 --db-user Админ --uccode tester
 if "%1"=="restore"      call vrunner restore "D:\\Vanessa-Automation\\DT\\%2.dt" --ibconnection /Slocalhost/%3 --uccode tester
 if "%1"=="remove"       call vrunner remove --name %2 --drop-database --clear-database --db-user Админ --uccode tester
@@ -14,7 +14,7 @@ if "%1"=="updatedb"     call vrunner updatedb --ibconnection /Slocalhost/%2 --db
 if "%1"=="vanessa"      call vrunner vanessa --path "%2%3" --vanessasettings "tools/VAParams.json" --workspace tests --pathvanessa %4 --additional "/DisplayAllFunctions /L ru" --ibconnection /Slocalhost/%5 --db-user Админ --uccode tester
 if "%1"=="update1C"     call vrunner run --command ЗавершитьРаботуСистемы; --ibconnection /Slocalhost/%2 --db-user Админ --execute "C:\\Program Files\\OneScript\\lib\\vanessa-runner\\epf\\ЗакрытьПредприятие.epf" --uccode tester
 if "%1"=="dump"         call vrunner dump "D:\\Vanessa-Automation\\DT\\%2.dt" --ibconnection /Slocalhost/%3 --db-user Админ --uccode tester
-if "%1"=="session_unlock" call vrunner session unlock --ras serverRAC --db %2 --db-user Админ --uccode tester
+if "%1"=="session_unlock" call vrunner session unlock --ras "C:\Program Files\1cv8\8.3.27.1688\bin\rac.exe" --db %2 --db-user Админ --uccode tester
 if "%1"=="run"          call vrunner run --ibconnection /Slocalhost/%2 --db-user Админ --execute "C:\\Program Files\\OneScript\\lib\\vanessa-runner\\epf\\УбратьОкноПеремещенияИБ.epf" --uccode tester
 if "%1"=="smoke"        call vrunner run --command "VAParams=%2\\tests\\tools\\VAParams.json;StartFeaturePlayer;GenerateSmokeTest;QuietInstallVanessaExtAndClose" --execute "%3" --ibconnection /Slocalhost/%4 --db-user Админ --uccode tester
 if "%1"=="extension"    call vrunner loadrepo --storage-name %2 --storage-user МихаилБ --extension %3 --ibconnection /Slocalhost/%4 --db-user Админ --uccode tester
